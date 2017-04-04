f1362593e80c83bc818144b693a6cd290b5c0b08
Your Name <ahmedrahmy842@gmail.com>
2017-04-04 13:46:29 +0200

    Revert "Added mt-plat directory"
    This reverts commit a90cfedc86f1746038cc08d9b8216f44c66a1ba1.
    It might be causing errors

--- include/mt-plat/mt_debug_latch.h
+++
@@ -1,8 +0,0 @@
-#ifndef __MT_DEBUG_LATCH_H__
-#define __MT_DEBUG_LATCH_H__
-
-/* public APIs for those want to dump lastbus to buf */
-int __attribute__((weak))	mt_lastbus_dump(char *buf);
-
-
-#endif /* end of __MT_DEBUG_LATCH_H__ */
