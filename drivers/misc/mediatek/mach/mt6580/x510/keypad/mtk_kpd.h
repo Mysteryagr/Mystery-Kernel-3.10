195aee0bf774691423689ed14f9c83f0a7ef5c43
Ahmed Rahmy <ahmedrahmy842@gmail.com>
2017-04-09 22:29:00 +0200

    Trying to fix volume button #2

--- drivers/misc/mediatek/mach/mt6580/x510/keypad/mtk_kpd.h
+++ drivers/misc/mediatek/mach/mt6580/x510/keypad/mtk_kpd.h
@@ -74,7 +74,7 @@ void kpd_pmic_rstkey_handler(unsigned long pressed);
 //#define EVB_PLATFORM
 #define ONEKEY_REBOOT_NORMAL_MODE
 //#define ONEKEY_REBOOT_OTHER_MODE
-#define KPD_PMIC_RSTKEY_MAP KEY_VOLUMEUP
+#define KPD_PMIC_RSTKEY_MAP KEY_VOLUMEDOWN
 #define KPD_PMIC_LPRST_TD 2 /* timeout period. 0: 8sec; 1: 11sec; 2: 14sec; 3: 5sec */
 
 #endif
